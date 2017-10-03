08e79ad3b25bbe566a81592143610ee0277ae3cb
Bisher Anadani <bisher.anadani@ku.edu>
2017-10-02 23:47:56 -0500

    Exercise 2

--- helloWorld.php
+++ helloWorld.php
@@ -1,13 +1,40 @@
-<?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Multiplier</title>
+  <style media="screen">
+    th, td{
+      border: 1px solid black;
+    }
+    td{
+      padding: 2px;
+    }
+  </style>
+</head>
+<body>
+  <?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
 
-for($i=1; $i<=10; $i++){
-  for($j=1; $j<=10; $j++){
-    $l = $i*$j;
-    echo "$l &emsp;";
+  echo "<h1>" . "Multiplication table 100 x 100" . "</h1>";
+
+  echo "<table>";
+
+  for($i=1; $i<=100; $i++){
+    echo "<tr>";
+    for($j=1; $j<=100; $j++){
+      $l = $i*$j;
+      echo "<td>" . "$l" . "</td>";
+    }
+    echo "</tr>";
   }
-  echo "<br>";
-}
 
-?>
+  echo "</table>";
+
+  ?>
+
+</body>
+</html>
--- quiz.php
+++ quiz.php
@@ -1,9 +1,28 @@
 <?php
 //access the global array called $_POST to get the values from the text fields
-$name = $_POST["name"];
-$email = $_POST["email"];
+$labels = array($_POST["color-label"],$_POST["city-label"],$_POST["drink-label"],$_POST["animal-label"],$_POST["lang-label"]);
+$corrects = array("Green", "New York", "Juice", "Hippopotamus", "C++");
+$answers = array($_POST["color"],$_POST["city"],$_POST["drink"],$_POST["animal"],$_POST["lang"]);
+$score = 0;
 
-echo "Name: " . $name . "<br>";
-echo "Email: " . $email . "<br>";
+for($i=0; $i<5; $i++){
+  echo "Question: " . $labels[$i] . "<br>";
+  echo "Your Answer: " . $answers[$i] . "<br>";
+  echo "Correct Answer: " . $corrects[$i] . "<br>";
+  $j = $i+1;
+  if($answers[$i] == $corrects[$i]){
+    $score =$score+20;
+    echo "You got question " . "$j" . " right!";
+    echo "<br>";
+  }
+  else{
+    echo "You got question " . "$j" . " wrong, Sad!";
+    echo "<br>";
+  }
+
+  echo "<br>";
+}
+
+echo "Final score: " . "<b>" . "$score" . "% </b>";
 
 ?>
